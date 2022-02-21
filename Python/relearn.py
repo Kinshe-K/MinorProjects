61612b378f436684f8e976595fa3d0eca3c5c2ac
Kinshe-K <qkelley0726@gmail.com>
2022-02-21 12:34:36 -0500

    Started working on the Relearn document. Added basic print and input statements.

---
+++ Python/relearn.py
@@ -0,0 +1,5 @@
+import os; ##Incase it isneeded
+
+print("Hello again world, Im back") #Lets start with this
+val = input("Can you please tell me your name again? \n> ") 
+print("Welcome Back " + val + "!")
