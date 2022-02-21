a3e3b1b4a7a96525525be0c004735ada48d95411
Kinshe-K <qkelley0726@gmail.com>
2022-02-21 13:25:08 -0500

    Made the Datastructuring folder for the datastructuring project.

---
+++ Python/DataStructuring/node.py
@@ -0,0 +1,11 @@
+"""
+Node structure file
+Basic peice that will be called on by Stack and queue
+Meant to save writing on the document
+"""
+
+class node:
+
+    __slots__ = [__val__,__next__]
+
+    
