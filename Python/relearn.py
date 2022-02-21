a94e0eb8e2a63409b4f4da8bfe842cd97909194c
Kinshe-K <qkelley0726@gmail.com>
2022-02-21 12:43:43 -0500

    Started making a function, flips a string

--- Python/relearn.py
+++ Python/relearn.py
@@ -1,5 +1,15 @@
 import os; ##Incase it isneeded
 
+def nameflip(string:str): 
+    flip = "";
+    for i in range((string.__len__() - 1),0,-1):
+        flip += string[i];
+    return flip;
+
+
 print("Hello again world, Im back") #Lets start with this
 val = input("Can you please tell me your name again? \n> ") 
 print("Welcome Back " + val + "!")
+val = nameflip(val)
+print("Flipped: " + val)
+
